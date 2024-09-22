268a5391a1d4e8942249970aa5c0f06c832f55aa
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2024-09-22 13:46:40 +0700

    update: migartion, model, controller

---
+++ database/migrations/2024_09_22_062806_create_movies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('release_date')->nullable();
+            $table->integer('duration')->nullable(); // Duration in minutes
+            $table->decimal('rating', 3, 1)->nullable(); // Rating, e.g., 8.5
+            $table->foreignId('director_id')->nullable()->constrained('directors')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
